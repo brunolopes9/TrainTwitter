6fb2344cf8333ca0d71857a8bfd370a0759a6de9
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-07-29 23:12:18 +0100

    add styles

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,11 +1,16 @@
+import styles from "./Tweet.module.css"
+
 export default function Tweet(props) {
   return (
     <div className={styles.tweetContainer}>
-      <img src={"https://github.com/brunolopes9.png"} />
+      <img
+        className={styles.avatar}
+        src={"https://github.com/brunolopes9.png"}
+      />
       <div className={styles.user}>
-        <span> Bruno Lopes </span>
-        <span> @brunolopes</span>
-        <span> 12 Hours Ago</span>
+        <span className={styles.userName}> Bruno Lopes </span>
+        <span className={styles.userUserName}> @brunolopes</span>
+        <span className={styles.date}> 12 Hours Ago</span>
       </div>
       <div className={styles.tweetText}> {props.children} </div>
     </div>
